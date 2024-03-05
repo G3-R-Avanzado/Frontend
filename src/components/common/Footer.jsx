1331ce5f275d30a32b5450859569f9623b19cdec
Mauricio Soria <mauricioutn2017@gmail.com>
2024-03-05 17:26:01 -0300

    fix footer style

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <footer className="d-flex flex-column py-5 bg-white">
+        <footer className="py-5 bg-white">
         <Container >
             <h5 className='text-black text-center'>&copy; Todos los derechos reservados</h5>
             <h6>Tributo a Mercado Pago</h6>
