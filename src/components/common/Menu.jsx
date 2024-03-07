fc27d822951ed4b8b2324de953f123e15bbf5583
Mauricio Soria <mauricioutn2017@gmail.com>
2024-03-07 00:58:44 -0300

    Agregado boton de inicio de navegacion

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -4,12 +4,15 @@ import Logo from "../../assets/logo1.png"
 import { NavLink, useNavigate } from 'react-router-dom';
 
 const Menu = (props) => {
+    const nav=useNavigate();
     return (
         <>
             <Navbar expand="md" className="navbarStyle site-wrap" >
-                    <Navbar.Brand  className='d-flex text-white'>
+                    <Navbar.Brand  className='d-flex text-white' >
+                        <NavLink className={"text-decoration-none d-flex"} to="/home">
                         <Image src={Logo} style={{ height: "50px" }} alt="" className='mx-2 mt-2 ' roundedCircle />
                         <p className='ms-2 mt-3 text-black fs-5'>TucuLibre </p>
+                        </NavLink>
                     </Navbar.Brand>
                     <Navbar.Toggle className='text-white' aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse className='responsive-navbar-nav'>
